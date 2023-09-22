3184ef43bce53d94309f19bf95ae546b12fa4cd8
LuciaMG <145746996+LuciaMartinG@users.noreply.github.com>
2023-09-22 11:09:48 +0200

    Python_Dia1

---
+++ Dia1.py
@@ -0,0 +1,33 @@
+#Que bonito es Python
+print("Hola Mundo")
+print ("Me llamo Lucia")
+#Nuestra primera variable
+x = 5
+print ("print (5):")
+print (5)    #Imprime 5 sin necesidad de comillas.
+print ("print (5+5):")
+print (5+5)   #Imprime 10.
+print ("print (x):")
+print (x)     #5
+print ("print (x+10):")
+print (x+10)
+#Creamos una variable con texto -> string
+nombre = "Lucia"
+numero = 8
+print (nombre)
+print ("Sumo nombre+numero:")
+print ('print ("Lucia"+"8")')
+print ("Lucia"+"8")
+print ('print (nombre+str(numero)):')
+print (nombre+str(numero))
+print ("Escribo 10 veces CuCu")
+print ("CuCu"*10)
+print ("\nHacemos operaciones matemáticas")
+print ("\n\\Hacemos \"operaciones\"\t matematicas")
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta = num1-num2
+multp = num1*num2
+div = num1/num2
+print ("Suma = " + str(suma)+ " | Resta = " + str (resta) + "| Multiplicacion = " + str(multp) + "| Division = " + str (div))
